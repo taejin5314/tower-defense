2bc99eed52ebc7ae0990024939d4d16e819da382
taejin <taejin5314@gmail.com>
2021-04-26 16:25:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = 900;
 canvas.height = 600;
+
+// global variables
+// game board
+// projectiles
+// defenders
+// enemies
+// resources
+// utilities
