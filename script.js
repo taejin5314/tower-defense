a44e7fa47a1aa2d0ef12445e95cd9313554fdf79
taejin <taejin5314@gmail.com>
2021-04-29 16:22:34 -0400

    working on resources

--- script.js
+++ script.js
@@ -222,7 +222,18 @@ function handleEnemies() {
     if (enemiesInterval > 120) enemiesInterval -= Math.floor(Math.random() * 40);
   }
 }
+
 // resources
+const amounts = [20, 30, 40];
+class Resource {
+  constructor() {
+    this.x = Math.random() * (canvas.width - cellSize);
+    this.y = (Math.floor(Math.random() * 5) + 1) * cellSize + 25;
+    this.width = cellSize * 0.6;
+    this.height = cellSize * 0.6;
+    this.amount = amounts[Math.floor(Math.random() * amounts.length)];
+  }
+}
 // utilities
 function handleGameStatus() {
   ctx.fillStyle = 'white';
